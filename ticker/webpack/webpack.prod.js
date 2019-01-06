abe922c3684b889918c16654fd35c7a4535c2f79
Kolya Venturi <kolya.venturi@enx3s.com>
2019-01-06 15:55:23 -0700

    Change to terser plugin instead of uglify

--- ticker/webpack/webpack.prod.js
+++ ticker/webpack/webpack.prod.js
@@ -1,16 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      })
-    ]
+    minimizer: [new TerserPlugin()]
   }
 });
